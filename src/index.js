9293a32a77c6d3a87f66a933ad0d0a354aa9879a
Iryna <irasokolova25@ukr.net>
2021-09-22 13:22:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,76 @@
+
 import './sass/main.scss';
-import './js/refs';
-import './js/api-service';
-import './js/scroll';
-// import icons from '.material-design-icons';
-// console.log(icons)
+import refs from './js/refs';
+import ApiService from './js/api-service';
+import LoadMoreBtn from './js/load-more-btn';
+import gallaryTpl from './templates/gallary.hbs';
 import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
 import '@pnotify/core/dist/BrightTheme.css';
 import '@pnotify/core/dist/PNotify.css';
 defaultModules.set(PNotifyMobile, {});
 
-alert({
-  text: 'Opps!'
+
+
+const {gallaryList, searchForm, gallaryItem, photoCard, searchBtn, loadMoreBtn} = refs;
+
+const apiService = new ApiService();
+const loadMoreButton = new LoadMoreBtn({
+  selector: '[data-action="load-more"]'
 });
 
 
+searchForm.addEventListener('submit', onSearch);
+loadMoreButton.refs.button.addEventListener('click', onLoadMore);
 
+function onSearch(e){
+    e.preventDefault();
+    apiService.query = e.currentTarget.elements.query.value.trim();
+    if(!apiService.query)
+    return alert({
+            text: 'Opps! No request! Try again!',
+            type: 'error',
+            delay: 1000,
+            hide: true,
+          });
 
+    // if(hits.lenght === 0)
+    // return alert({
+    //         text: 'Opps! No request! Try again!',
+    //         type: 'error',
+    //         delay: 1000,
+    //         hide: true,
+    //       });
+    loadMoreButton.show();
+    apiService.clearPage();
+    clearGallaryList();
+    fetchHits();
+    
+    }
+    
+function onLoadMore(){
+  fetchHits();
+  gallaryList.scrollIntoView({
+  behavior: 'smooth',
+  block: 'end',
+});
 
+    }
+
+function fetchHits(){
+  loadMoreButton.disable();
+     apiService.fetchImg().then(hits => {
+      createGallaryList(hits);
+      loadMoreButton.enable();
+     });
+}
+
+function createGallaryList(hits){
+  gallaryList.insertAdjacentHTML('beforeend', gallaryTpl(hits));
+}
+
+function clearGallaryList(){
+  gallaryList.innerHTML = '';
+}
+
+ 
