2cc3c96ba1e3c529e9d63903b4508e97efc6cc3b
Iryna <irasokolova25@ukr.net>
2021-09-22 13:22:52 +0300

    Update api-service.js

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,28 +1,36 @@
-// export default
-//  apiService = {
-
-// }
-// Есть файл apiService.js с дефолтным экспортом объекта 
-// отвечающего за логику HTTP-запросов к API
-import gallaryTpl from '../templates/gallary.hbs';
-const BASE_URL = 'https://pixabay.com/api/';
+const url = 'https://pixabay.com/api/';
 const MY_KEY = '23478733-19045e71a96dc7e272282171b';
-let query = '';
-let pageNamber = 1;
 
-function searchImg(query, pageNamber){
-   
-   return fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${query}&page=${pageNamber}&per_page=12&key=${MY_KEY}`)
-    .then(r=>r.json()
-    .then(console.log))
+
+export default
+class ApiService {
+    constructor(){
+        this.query = '';
+        this.page = 1;
+    }
+fetchImg(){
+return fetch(`${url}/?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${MY_KEY}`)
+    .then(r=>r.json())
+    .then(({hits}) => {
+        this.incrementPage();
+        return hits;
+    })
 }
 
-function onPageIncrement(){
-    pageNamber +=1
-};
-function onPageNumberClear(){
-    pageNamber = 1;
+incrementPage(){
+    this.page +=1
 }
-function createGallaryList(){
-    gallaryList.innerHTML = gallaryTpl(array);
+ 
+clearPage(){
+    this.page = 1;
 }
+
+// get searchQuery(){
+//     return this.query;
+// }
+
+// set searchQuery(searchQuery){
+//     this.query = searchQuery;
+// }
+}
+
