9a82645ce5782a5305136236d5ddef6fee4ae97b
Iryna <irasokolova25@ukr.net>
2021-09-27 22:38:28 +0300

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 
-import regeneratorRuntime from "regenerator-runtime";
+// import regeneratorRuntime from "regenerator-runtime";
+import * as basicLightbox from 'basiclightbox'
 import './css/styles.css';
 import refs from './js/refs';
+// import './js/basic-lightbox';
 import ApiService from './js/api-service';
 import LoadMoreBtn from './js/load-more-btn';
 import galleryTpl from './templates/gallery.hbs';
@@ -14,20 +16,53 @@ defaultModules.set(PNotifyMobile, {});
 
 
 const {galleryList, searchForm} = refs;
+console.log(refs);
+// const apiService = ApiService;
+// console.log(apiService);
+
+
 
 const apiService = new ApiService();
+// console.log(apiService);
 const loadMoreButton = new LoadMoreBtn({
   selector: '[data-action="load-more"]',
   hidden: true,
 });
+// searchForm.addEventListener('submit', onSearch);
+// function onSearch(e){
+//     console.log(e);
+//     e.preventDefault();
+//     apiService.query = e.currentTarget.elements.query.value.trim();
+//     console.log(apiService.query);
+//     if(!apiService.query){
+//             return alert({
+//             text: 'Opps! No request! Try again!',
+//             type: 'error',
+//             delay: 1000,
+//             hide: true,
+//             })
+//             }; 
+//       apiService.fetchImg();
+//       loadMoreButton.disable();
+// }
 
+// loadMoreButton.refs.button.addEventListener('click', onLoadMore);
+// function onLoadMore(){
+//       loadMoreButton.disable();
+//          apiService.fetchImg();
+//         loadMoreButton.enable();
+          
+// }
+      
 
 searchForm.addEventListener('submit', onSearch);
-loadMoreButton.refs.button.addEventListener('click', onLoadMore);
+loadMoreButton.refs.button.addEventListener('click', onFetch);
 
 function onSearch(e){
  e.preventDefault();
+
   apiService.query = e.currentTarget.elements.query.value.trim();
+  
 if(!apiService.query){
       return alert({
         text: 'Opps! No request! Try again!',
@@ -36,39 +71,64 @@ if(!apiService.query){
         hide: true,
       })
     }; 
-    
-    apiService.fetchImg() 
-.then(hits =>{
-    if(hits.length === 0){
-      return alert({
-        text: 'Opps! Invalid request! Try again!',
-        type: 'error',
-        delay: 1000,
-        hide: true,
-      });
-    }       
-    if (hits.length >=1 ){
-      createGalleryList(hits);
-      smoothScroll();
-      loadMoreButton.enable();
-      loadMoreButton.show();
-    }
-      })
-      loadMoreButton.disable();
-    
+    loadMoreButton.show();
     apiService.clearPage();
     clearGalleryList();
-    searchForm.reset();  
+    onFetch();
+//     console.log(apiService.fetchImg())
+ 
+//   apiService.fetchImg() 
+//   .then(hits =>{
+//   console.log(hits);
+//     if(hits.length === 0){
+//       return alert({
+//         text: 'Opps! Invalid request! Try again!',
+//         type: 'error',
+//         delay: 1000,
+//         hide: true,
+//       });
+//     }
+// loadMoreButton.disable();
+//     if (hits.length >=1 ){
+//       createGalleryList(hits);
+//       smoothScroll();
+//       loadMoreButton.enable();
+      
+      
+//     }})
+    
+      
+      
+     
+//       loadMoreButton.show();
+//     // loadMoreButton.disable();
+//     apiService.clearPage();
+//     // console.log(apiService.clearPage());
+//     clearGalleryList();
+//     searchForm.reset();  
 }
   
-function onLoadMore(){
+function onFetch(){
   loadMoreButton.disable();
-     apiService.fetchImg().then(hits => {
+     apiService.fetchImg()
+     .then(hits => {
       createGalleryList(hits);
       smoothScroll();
       loadMoreButton.enable();
+      if(hits.length === 0){
+        
+              return alert({
+                text: 'Opps! Invalid request! Try again!',
+                type: 'error',
+                delay: 1000,
+                hide: true,
+              });
       
-     });
+     }
+    })
+     .catch(error=>{
+       console.log(error);
+     })
   }
 
 function smoothScroll(){
@@ -87,3 +147,13 @@ function clearGalleryList(){
   galleryList.innerHTML = '';
 }
 
+// Большое изображение
+
+galleryList.addEventListener('click', (e)=>{
+  console.log(e);
+if(e.target.img){
+  basicLightbox.create(`
+		<img src=${e.target.dataset['source']} width="1400" height="900">
+	`).show()
+}
+})
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,7 +1,89 @@
+import { async } from 'regenerator-runtime';
+// import refs from './refs';
+// const {galleryList, searchForm, input} = refs;
+// console.log(input);
 const url = 'https://pixabay.com/api/';
 const MY_KEY = '23478733-19045e71a96dc7e272282171b';
+// console.dir(searchForm);
+// console.log(searchForm.nodeValue);
+
+// export default 
+//  apiService = {
+    
+//     query : '',
+//     page : 1,
+//     key : '23478733-19045e71a96dc7e272282171b',
+//     url :'https://pixabay.com/api/',
+//     incrementPage(){
+//         this.page +=1;
+//         console.log(this);
+//     },
+//     clearPage(){
+//         this.page = 1;
+//     },
+
+//     async fetchImg (){
+//         // console.log(e);
+//         // e.preventDefault();
+//         // query = e.currentTarget.elements.query.value.trim();
+//         // console.log(query);
+//         // if(!query){
+//         //         return alert({
+//         //         text: 'Opps! No request! Try again!',
+//         //         type: 'error',
+//         //         delay: 1000,
+//         //         hide: true,
+//         //         })
+//         //         }; 
+//         let response = await fetch(`${this.url}?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${this.key}`);
+//         if (response.status === 404){
+//                 return alert({
+//                 text: 'Not found!',
+//                 type: 'error',
+//                 delay: 1000,
+//                 hide: true,
+//                 })
+//             }
+//         let hits = await response.json();
+//         if (hits.length === 0){
+//                 return alert({
+//                 text: 'Opps! Invalid request! Try again!',
+//                 type: 'error',
+//                 delay: 1000,
+//                 hide: true,
+//                 });
+//         }
+        
+//         this.incrementPage()
+//         console.log(hits);
+//         if (hits.length >= 1 ){
+//             galleryList.insertAdjacentHTML('beforeend', galleryTpl(hits));
+//             galleryList.scrollIntoView({
+//                     behavior: 'smooth',
+//                     block: 'end',
+//                   });
+//                 // loadMoreButton.enable();
+//                 // loadMoreButton.show();
+//             }
+//         this.clearPage(),
+//         galleryList.innerHTML = '';
+//         searchForm.reset();
 
 
+//     }
+// }
+//  console.log(apiService);
+ 
+
+
+        
+//         // this.incrementPage();
+//         // return hits;
+//         //      }
+//         //      catch(error){
+//         //          console.log(error)},
+
+  
 export default
 class ApiService {
     constructor(){
@@ -9,15 +91,15 @@ class ApiService {
         this.page = 1;
     }
 async fetchImg(){         
-     try{
-       let response = await fetch(`${url}/?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${MY_KEY}`);
-        let data = await response.json();
-        let hits = await data.hits 
-        this.incrementPage();   
-        return hits;
-     }
-     catch(error){
-         console.log(error)}          
+    let response = await fetch(`${url}/?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${MY_KEY}`);
+    let data = await response.json();
+    let hits = await data.hits
+    this.incrementPage();
+    // console.log(hits.length);
+    
+    return hits;
+    
+            
             
 
 
@@ -33,10 +115,11 @@ async fetchImg(){
 
 incrementPage(){
     this.page +=1
+    
 }
  
 clearPage(){
-    this.page = 1;
+    return this.page = 1;
 }
 }
  
