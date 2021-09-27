ba2eb118aac2f8fa198ee8b31b4b2508718d6ecd
Iryna <irasokolova25@ukr.net>
2021-09-27 22:38:57 +0300

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,8 +1,6 @@
 export default {
     galleryList: document.querySelector('.gallery'),
     searchForm: document.getElementById('search-form'),
-    galleryItem: document.querySelector('.gallery-item'),
-    photoCard: document.querySelector('.photo-card'),
     searchBtn: document.querySelector('.btn-primary'),
     loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 }
